416523cee8fe8fa504e4510b5b4a620e35f4db34
preeti singh <singhpreeti236@gmail.com>
2015-08-14 05:44:59 -0400

    Added the files for tracking traffic using OpenCV, with a interface built in Javascript

---
+++ server/static/app.js
@@ -0,0 +1,310 @@
+var route_line = "}`iuF`o}gN_@l@uCqBs@e@w@~BiAdDoBvFaHgCuDkAwIsCqBo@_@MOQw@Y_@UoA_AuAmA_@c@k@s@o@kA_DqGK?}LmX}AeDi@q@]c@g@_@sDmCiG{D{Bw@[GcB[QEQpCMnBQjAUp@_@fAo@~@YV_@Rs@Rq@Bm@Ei@KaAi@u@g@y@o@oAe@{@oAgAeA{OwO_D}Ce@YuCeAm@SeAQaBOqB@gCJcCj@eD`AoLxDiMpEyHhC_GxBwB|@_B|@oH~E{B|A{IhHm@h@wA`As@f@QNqBxAaDdBaD~@u@N_LtAqW~CyGp@qAJwA?yAIeBWyAc@gBq@wBoAuCsBgL_JwB}AeDeBwAg@oCe@wAIoBAkCViCv@uAj@wBhAuAdA_HhFuBlAc@Ne@Jm@NsANw@_@YWWc@GYIi@Aq@Lq@f@aA\\O\\G\\?l@Rf@f@Xz@Dd@Ex@OjBO~AaAjK?f@wBhWy@xJ]zFCvBEnH?fIGdFQxEm@tGa@pCg@hCaAjEmCfIsCzIgHdTkFdPuCpIyFjP{@rBuFdN}CtHyEbLmDpI}AhEwH|QiCtGeNr\\mKvWsCtF}B|CeBpBwBnBoCjBiBjA{InFmCdBeCjB_BjBwA|CSn@k@j@_@j@m@`BUfA[tCMZMVSPi@T_AQk@Ug@_@y@eAmCaEyA}BmAwC";
+//TODO add 65-100 for 379
+
+
+window.cameraMarkers = {};
+window.polylines = {};
+window.labels = {};
+window.MPH_TO_METERSMIN = (26.82);
+window.NUM_LANES = 4;
+window.a = setTimeout(undefined, 1000000000000000000000);
+window.start_id = null;
+window.end_id = null;
+
+var currentcamera = 595;
+var url_base = 'http://www.dot35.state.pa.us/public/Districts/District11/WebCams/D11-';
+//plot the current (preprogrammed) route
+function plot_preprogrammed_route()
+{
+    var route = google.maps.geometry.encoding.decodePath(route_line)
+    var start = route[0];
+    var end = route[route.length - 1];
+    var start = new google.maps.Marker({
+        position: start,
+        map: map,
+        title: "Route Start"
+    });
+    var end = new google.maps.Marker({
+        position: end,
+        map: map,
+        title: "Route End"
+    });
+    var poly = new google.maps.Polyline({
+        map: map,
+        path: route,
+        strokeColor: '#0000FF',
+        strokeOpacity: 0.8,
+        strokeWeight: 3
+    });
+}
+
+//plot the route given by start and end
+function plot_route() 
+{
+    if (start_id == null || end_id == null) {
+        console.log("Cannot calculate; empty start and end");
+        return;
+    }
+    start_latlng = new google.maps.LatLng(cameras[start_id].lat, cameras[start_id].lng);
+    end_latlng = new google.maps.LatLng(cameras[end_id].lat, cameras[end_id].lng);
+    getDirections(start_latlng, end_latlng);
+}
+
+//calculate the travel time of the current route
+function travel_time()
+{
+    //waypoints 50-90 are on I-79 and have camera coverage
+    var route = google.maps.geometry.encoding.decodePath(route_line);
+    var time = 0;
+    for (var i = 0; i < route.length - 1; i++) {
+        if (i >= 50 && i < 90) {
+            for (var j = 550; j < 595; j+=5) {
+                var cam1 = new google.maps.LatLng(cameras[j].lat,cameras[j].lon);
+                var cam2 = new google.maps.LatLng(cameras[j+5].lat,cameras[j+5].lon);
+                var d1 = google.maps.geometry.spherical.computeDistanceBetween(route[i],cam1)
+                var d2 = google.maps.geometry.spherical.computeDistanceBetween(route[i],cam2)
+                var cam = d1 < d2 ? j : j+5;
+                var s = currentstats[cam].count;
+                var dist = google.maps.geometry.spherical.computeDistanceBetween(route[i], route[i+1])/10
+                time += dist/(speed(s) * MPH_TO_METERSMIN);
+            }
+        } else {
+            //no camera, just guess
+            var s = 35;
+            var dist = google.maps.geometry.spherical.computeDistanceBetween(route[i], route[i+1])
+            time += (dist)/(s * MPH_TO_METERSMIN);
+        }
+    }
+    return time;
+}
+// Use the google directions api to obtain a route between two locations
+function getDirections(start, end)
+{
+    if (!window.dir)
+        window.dir = new google.maps.DirectionsService();
+    dir.route({
+        destination: end,
+        origin: start,
+        provideRouteAlternatives: false,
+        unitSystem: google.maps.UnitSystem.METRIC,
+        travelMode: google.maps.TravelMode.DRIVING, 
+        avoidHighways: false,
+        avoidTolls: false
+    }, function(result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(result);
+        }
+    });
+
+}
+//callback invoked when we need to update the camera image and marker
+//if id is given, go directly there. otherwise, move to next
+function newcamera(id)
+{
+    cameraMarkers[currentcamera].setIcon(null);
+    id = id || cameras[currentcamera].next;
+    cameraMarkers[id].setIcon('http://www.google.com/uds/samples/places/temp_marker.png');
+    currentcamera = id;
+    var img = document.getElementById('camera_image');
+    img.src = url_base + id + '.jpg?v=' + new Date().getTime();
+    if (a)
+        clearTimeout(a);
+    window.a = setTimeout(newcamera,5000);
+}
+//calculate the road line color for a given car count
+function calcColor(carcount)
+{
+    //if (carcount > 30)
+    if (carcount > 5)
+        return '#FF0000';
+    if (carcount > 10 && carcount <= 20)
+        return '#FFFF00';
+    else
+        return '#00FF00';
+}
+//calculate the speed given the car count
+function speed(carcount)
+{
+    //if (carcount <=20)return 65;
+    if (carcount <= 5) return 65;
+    return ((10*NUM_LANES - carcount) * 3);
+}
+//calculate the time between two cameras on the current I-79 route
+function time_between(cam1, cam2)
+{
+    if (cameras[cam1] === undefined || cameras[cam2] === undefined)
+        throw new Error ("Camera not in range");
+    var time = 0;
+    var len = 0;
+    for (var i = parseInt(cam1); i < parseInt(cam2); i = cameras[i].next) {
+        var curr_line = google.maps.geometry.encoding.decodePath(lines[i+ "-" + cameras[i].next].polyline);
+        var line_len = google.maps.geometry.spherical.computeLength(curr_line)
+        len += line_len
+        time += line_len/(speed(window.currentstats[cam1].count) * MPH_TO_METERSMIN);
+    }
+    return time;
+}
+//pull updated stats from the server side processor
+function stats()
+{
+    $.getJSON('/endpoint?callback=?', function(data) {
+        window.currentstats = data;
+        for (var i in data) {
+            if (i !== '710' && i.indexOf('-') < 0) {
+                var str = i+'-'+cameras[i].next;
+                var line = polylines[str];
+                var newColor = calcColor(data[i].count);
+                if (newColor !== line.strokeColor) {
+                    console.log("MADE IT");
+                    line.strokeColor = newColor;
+                    line.changed();
+                }
+                labels[str].text = speed(data[i].count)+ "mph (" + data[i].count + " cars)";
+                labels[str].changed('text');
+            }
+        }
+        //document.getElementById("tt").innerText = Math.round(travel_time()) + " minutes";
+        if (start_id == null || end_id == null) {
+            return;
+        }
+        document.getElementById("tt").innerText = Math.round(time_between(start_id, end_id)) + " minutes";
+    });
+}
+
+function handle_events()
+{
+    // If input cleared, change to .input_highlighted; clear text
+    $("#clear").click(function() {
+        $(".textbox").val("");
+        $(".textbox#start").addClass("input_highlighted");
+        window.start_id = null;
+        window.end_id = null;
+        directionsDisplay.setMap(null); 
+        directionsDisplay = null;
+        directionsDisplay = new google.maps.DirectionsRenderer();
+        directionsDisplay.setMap(map);
+    });
+
+    // Handle expanding and collapsing input details on arrow click
+    $(".arrow").click(function(e) {
+        console.log(e.target);
+        if ($(".arrow").hasClass("minimized")) {
+            $("#input_block").removeClass("hidden");
+            $(".arrow").removeClass("minimized");
+        } else {
+            $("#input_block").addClass("hidden");
+            $(".arrow").addClass("minimized");
+        }
+    })
+
+    // When calculate, rerun plot_route and recalculate travel time
+    $("#calculate").click(function() {
+        plot_route();
+        stats();
+    });
+
+}
+
+function get_camera_name(id) {
+    var all_cameras = cams['Location Name'].cameras;
+    for( var i = 0; i < all_cameras.length; i++) {
+        if (all_cameras[i].id == id)
+            return all_cameras[i].name;
+    }
+}
+
+window.onload = function() {
+    //setup map
+    var latlng = new google.maps.LatLng(40.39286439546028,-80.10119845581056);
+    var opt = {
+        zoom: 12,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    window.map = new google.maps.Map(document.getElementById("map_canvas"),opt);
+    window.directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsDisplay.setMap(map);
+
+    //place camera markers & listen for their clicks
+    for (var i in cameras) {
+        cameraMarkers[i] = new google.maps.Marker({
+            position: new google.maps.LatLng(cameras[i].lat,cameras[i].lng),
+            map: map,
+            title: i
+        });
+        cameraMarkers[i].info = new google.maps.InfoWindow({
+            content:    '<img border="0" align="left" src='+
+                        url_base+i+'.jpg?v='+new Date().getTime() + '>'
+        })
+
+
+        google.maps.event.addListener(cameraMarkers[i], 'click', function(event) {
+            $(".input_highlighted").val(get_camera_name(this.title));
+            var this_input = $(".input_highlighted");
+            var next_input = this_input.nextAll(".textbox");
+            if (next_input.size() > 0) {
+                window.start_id = this.title;
+                next_input.addClass("input_highlighted");
+            } else {
+                window.end_id = this.title;
+            }
+            this_input.removeClass("input_highlighted");
+
+        });
+
+        // Temporarily Open camera image if hover over marker
+        google.maps.event.addListener(cameraMarkers[i], 'mouseover', function(event) {
+            cameraMarkers[this.title].info.open(map, cameraMarkers[this.title]);
+
+            if (a)
+                clearInterval(a);
+            window.a = setInterval(
+                function() {
+                    cameraMarkers[this.title].info.setContent(
+                        '<img border="0" align="left" src='+url_base+this.title+'.jpg?v='+new Date().getTime() + '>'
+                    );
+                }.bind(this),5000);
+        });
+
+        // Close camera image when leave marker
+        google.maps.event.addListener(cameraMarkers[i], 'mouseout', function(event) {
+            if (a) clearInterval(a);
+            cameraMarkers[this.title].info.close();
+        });
+    }
+
+    
+    for (var i in lines) {
+        //draw camera to camera lines
+        polylines[i] = new google.maps.Polyline({
+            map: map,
+            path: google.maps.geometry.encoding.decodePath(lines[i].polyline),
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 7
+        });
+        //set up speed labels for each roadway
+        labels[i] = new MapLabel({
+            text: '',
+            position: new google.maps.LatLng(lines[i].labellat,lines[i].labellon),
+            map: map,
+            fontSize: 15,
+            align: 'left'
+        })
+    }
+    $.get('/static/cameras.xml', function(data) {
+        $(data).find("camera").each(function(i, elem) {
+            console.log("ID: " + $(this).find("id").text());
+            console.log("LAT: " + $(this).find("lat").text());
+            console.log("LON: " + $(this).find("lng").text());
+        })
+    });
+
+    //setup click handlers
+    handle_events();
+    //plot the test route
+    plot_route();
+    //start pulling in stats
+    stats();
+    setInterval(function() {stats()}, 5000);
+}
---
+++ server/static/lines.js
@@ -0,0 +1,137 @@
+var lines = {
+    '550-555': {
+        polyline: "_rjuFje_hNuTLiCGeBQuC{@",
+        labellat: 40.367932,
+        labellon:-80.117369
+    },
+    '555-560': {
+        polyline: "{skuFbc_hNcB}@uBeByAmBgAiBs@eBu@eCq@kFKuBGeBS{O",
+        labellat: 40.370613,
+        labellon: -80.113077
+    },
+    '560-565': {
+        polyline: "yeluFfr}gNUePSaD]_Cg@cCc@aBk@}Ak@mAgAaB",
+        labellat: 40.373097,
+        labellon: -80.099688
+    },
+    '565-570': {
+        polyline: "moluFjh|gNuAkBkCuCkGcG{@oAiAgAoSkS_Aw@_@QsDqAQC",
+        labellat: 40.378852,
+        labellon: -80.095224
+    },
+    '570-575': {
+        polyline: "w`nuF`zzgNs@MaBOqB@gCJcCj@eD`AoLxD}EdB",
+        labellat: 40.387089,
+        labellon: -80.094366
+    },
+    '575-580': {
+        polyline: "_kouF~e{gNkFjByHhC_GxBwCpA",
+        labellat: 40.392319,
+        labellon: -80.095396
+    },
+    '580-585': {
+        polyline: "}hpuF`t{gNyEvCqGnEiKrIuBxAm@b@cBlA",
+        labellat: 40.397418,
+        labellon: -80.098658
+    },
+    '585-590': {
+        polyline: "{lquFhp|gNGFaDdBaD~@u@N_LtA{XfD",
+        labellat: 40.405523,
+        labellon: -80.104237
+    },
+    '590-595': {
+        polyline: "y_suFd~|gNaIt@wA?yAIeBWyAc@gBq@wBoAuCsByD}CiCoB",
+        labellat: 40.411797,
+        labellon: -80.102434
+    },
+    '595-600': {
+    	polyline: "gktuFpm|gNiEuCyDwBuBs@eCa@_CCaBFyAJwAb@mDnAmE~CmIhGiDbAsBP{B?oFQ{GKqCN",
+    	labellat: 40.14122,
+    	labellon: -80.220414
+    },
+    '600-605': {
+    	polyline: "{cwuFxt|gN{Cv@{C|AoCnBiDjDiEhDoDvC",
+    	labellat:40.4316191 ,
+    	labellon:-80.108260
+    },
+    '605-610': {
+    	polyline: "gcxuFll}gNiFfC}E|A}EtAwFdAeIBqFg@{F]gUiBgGMeG?mNz@",
+    	labellat:40.44267677576393 ,
+    	labellon: -80.11108696460724
+    },
+    '610-612': {
+    	polyline: "}o{uFpt}gNmk@|BoLb@mCc@{JcA}G?}E~@",
+    	labellat:40.4597440385463 ,
+    	labellon:-80.11184334754944
+     },
+    '612-620': {
+    	polyline: "aj~uF~w}gNgJlDsRnMwHnFiMnFsVfJa_@rOc^nO",
+    	labellat:40.475584831989714,
+    	labellon:-80.11781126260757
+     },
+    '620-630': {
+    	polyline: "ywbvFlg`hNiIhCkHxCcEPcEuA{Ai@",
+    	labellat: 40.491483083864495,
+    	labellon:-80.1257050037384
+    },
+    '630-635': {
+        polyline:"szcvFbm`hNmCsDgB}DmA_C{AgB{BgCuB_BcEcAuDQiDr@eC`B",
+        labellat:40.497663473982335,
+        labellon:-80.1205632090568
+    },
+    '635-640': {
+        polyline:"odevF`u_hNcBjBqAbCi@fBm@lCc@rBMhBOrBE`B?~P",
+        labellat:40.50209,
+        labellon:-80.12624
+    },
+   '640-645': {
+        polyline:"eoevFzcahNC`]KhDa@rBs@lCgBtCcAlAkAv@iAl@_Bb@mBJ",
+        labellat:40.50234530141069,
+        labellon:-80.13573780655861
+    },
+    '645-655': {
+        polyline:"{bfvFb{bhNmACmCi@_IqBaf@uN??eFsAoEmAcFqAgA[{Bo@wD_AoDcAcEeAaFoA",
+	labellat:40.51132154427772,
+        labellon:-80.13541728258133
+    },
+    '655-660': {
+        polyline: "enivFrsahNyCg@_Da@iHYeDU_Ci@cBk@oBgAcCwB",
+        labellat: 40.52528,
+        labellon: -80.12895
+    },
+    '660-665': {
+        polyline: "gujvFbgahNaBaBuBkBaB}AyC}BsJqEiEaBcM}E",
+        labellat: 40.53245,
+        labellon: -80.12329
+    },
+    '665-670': {
+        polyline: "}clvFfi`hNcEgBuSeIiCcAoCs@{DUeBFeJn@eCCgBMyCg@",
+        labellat: 40.54217,
+        labellon: -80.11951
+    },
+    '670-675': {
+        polyline: "ahnvFhw_hNkBu@{BgA}AeAoDqCmEyCqL}I",
+        labellat: 40.54928,
+        labellon: -80.11779
+    },
+    '675-685': {
+        polyline: "wkovFz|~gNsCuB{GoBkLmByJcBgKeBgOiCiDUsDLuEh@aEpAqJrE",
+        labellat: 40.56076,
+        labellon: -80.1111
+    },
+    '685-690': {
+        polyline: "errvFzq~gNeB|@eCpAqBbAqBj@kCd@eD`@{I?iOCeCe@sCu@",
+        labellat: 40.57445,
+        labellon: -80.11522
+    },
+    '690-695': {
+        polyline: "gntvFby~gNi@c@y@m@m@g@q@s@e@k@i@s@k@_Au@_Bc@qA[mAeBcJgCsN}@uF{@oEuBuL",
+        labellat: 40.58032,
+        labellon: -80.11084
+    },
+    '695-710': {
+        polyline: "{juvF`p|gNo@mDgAeEu@wBgA{BkCgD{BkBeHmEaGeDqNwIyKiGuGkDyEcCiEqA{HqAcFUaHLqG`@aObAwWfBoN~@",
+        labellat: 40.59766,
+        labellon: -80.08913
+    }
+};
